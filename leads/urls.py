fcbd5a660a1126406712782b66452bf05307cc44
Konstantin Mogilevskii <kmogilevskii@outlook.com>
2022-09-12 12:36:26 +0400

    using base html page and link names

--- leads/urls.py
+++ leads/urls.py
@@ -4,9 +4,9 @@ from .views import lead_create, lead_detail, all_leads, update_lead, delete_lead
 app_name = "leads"
 
 urlpatterns = [
-    path('', all_leads),
-    path('create/', lead_create),
-    path('<int:pk>/', lead_detail),
-    path('<int:pk>/update/', update_lead),
-    path('<int:pk>/delete/', delete_lead)
+    path('', all_leads, name="lead-list"),
+    path('create/', lead_create, name="lead-create"),
+    path('<int:pk>/', lead_detail, name="lead-detail"),
+    path('<int:pk>/update/', update_lead, name="lead-update"),
+    path('<int:pk>/delete/', delete_lead, name="lead-delete")
 ]
