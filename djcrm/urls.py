8ed438501657f5cf9f78844c14f7666f18053fe4
Konstantin Mogilevskii <kmogilevskii@outlook.com>
2022-09-13 14:31:12 +0400

    templates and forms

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -15,9 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('leads/', include('leads.urls', namespace="leads"))
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- leads/forms.py
+++ leads/forms.py
@@ -1,5 +1,10 @@
 from django import forms
-from .models import Lead
+from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, UsernameField
+from .models import Lead, Agent, Category, FollowUp
+
+User = get_user_model()
 
 
 class LeadModelForm(forms.ModelForm):
@@ -9,11 +14,18 @@ class LeadModelForm(forms.ModelForm):
             'first_name',
             'last_name',
             'age',
-            'agent'
+            'agent',
+            'description',
+            'phone_number',
+            'email',
+            'profile_picture'
         )
 
 
-class LeadForm(forms.Form):
-    first_name = forms.CharField()
-    last_name = forms.CharField()
-    age = forms.IntegerField(min_value=0)
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username",)
+        field_classes = {
+            'username': UsernameField
+        }
