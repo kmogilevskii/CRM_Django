c5adc9f41f42cb9c09e1073fae90aee0f8f6f738
Konstantin Mogilevskii <kmogilevskii@outlook.com>
2022-09-14 15:47:32 +0400

    agent detail update delete

--- agents/views.py
+++ agents/views.py
@@ -40,3 +40,36 @@ class AgentCreateView(OrganisorAndLoginRequiredMixin, generic.CreateView):
             recipient_list=[user.email]
         )
         return super(AgentCreateView, self).form_valid(form)
+
+
+class AgentDetailView(OrganisorAndLoginRequiredMixin, generic.DetailView):
+    template_name = "agents/agent_detail.html"
+    context_object_name = "agent"
+
+    def get_queryset(self):
+        organisation = self.request.user.userprofile
+        return Agent.objects.filter(organisation=organisation)
+
+
+class AgentUpdateView(OrganisorAndLoginRequiredMixin, generic.UpdateView):
+    template_name = "agents/agent_update.html"
+    form_class = AgentModelForm
+
+    def get_success_url(self):
+        return reverse("agents:agent-list")
+
+    def get_queryset(self):
+        organisation = self.request.user.userprofile
+        return Agent.objects.filter(organisation=organisation)
+
+
+class AgentDeleteView(OrganisorAndLoginRequiredMixin, generic.DeleteView):
+    template_name = "agents/agent_delete.html"
+    context_object_name = "agent"
+
+    def get_success_url(self):
+        return reverse("agents:agent-list")
+
+    def get_queryset(self):
+        organisation = self.request.user.userprofile
+        return Agent.objects.filter(organisation=organisation)
