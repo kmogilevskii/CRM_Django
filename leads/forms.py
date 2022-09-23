8b75121a3a1646d01c54f6d5f11746a72e538444
Konstantin Mogilevskii <kmogilevskii@outlook.com>
2022-09-23 12:46:52 +0400

    slight changes

--- leads/forms.py
+++ leads/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UsernameField
 from .models import Lead, Agent, Category, FollowUp
